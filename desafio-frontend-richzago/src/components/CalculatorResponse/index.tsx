21354389b9edc2a8974f6a329bf26e9c89e7076d
richzago <rich.czago@gmail.com>
2023-01-17 13:25:24 -0300

    feat: created the response of calculator

---
+++ desafio-frontend-richzago/src/components/CalculatorResponse/index.tsx
@@ -0,0 +1,12 @@
+export const CalculatorResponse = () => {
+  return (
+    <div className="sectionTwo">
+      <h3>VOCÊ RECEBERÁ: </h3>
+      <hr></hr>
+      <p>Amanhã: Valor x</p>
+      <p>Em 15 dias: Valor x</p>
+      <p>Em 30 dias: Valor x</p>
+      <p>Em 90 dias: Valor x</p>
+    </div>
+  );
+};
