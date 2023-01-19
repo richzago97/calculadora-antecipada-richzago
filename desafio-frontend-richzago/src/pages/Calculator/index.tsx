951d6885268138366839182bdebb61faddb606f7
richzago <rich.czago@gmail.com>
2023-01-19 11:24:35 -0300

    feat: created styled components

--- desafio-frontend-richzago/src/pages/Calculator/index.tsx
+++ desafio-frontend-richzago/src/pages/Calculator/index.tsx
@@ -1,11 +1,12 @@
 import { CalculatorForm } from "../../components/CalculatorForm";
 import { CalculatorResponse } from "../../components/CalculatorResponse";
+import { StyledContainerPageCalculator } from "./style";
 
 export const CalculatorPage = () => {
   return (
-    <>
+    <StyledContainerPageCalculator>
       <CalculatorForm />
       <CalculatorResponse />
-    </>
+    </StyledContainerPageCalculator>
   );
 };
