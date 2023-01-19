5d67eff053b9101fc4c8694b8b2dbf1bb1e4a130
richzago <rich.czago@gmail.com>
2023-01-19 11:23:57 -0300

    feat: created interface IDays

--- desafio-frontend-richzago/src/interfaces/calculator.tsx
+++ desafio-frontend-richzago/src/interfaces/calculator.tsx
@@ -6,8 +6,8 @@ export interface IRequestForm extends IDays {
 }
 
 export interface IDays {
-  1: number;
-  15: number;
-  30: number;
-  90: number;
+  '1': number;
+  '15': number;
+  '30': number;
+  '90': number;
 }
