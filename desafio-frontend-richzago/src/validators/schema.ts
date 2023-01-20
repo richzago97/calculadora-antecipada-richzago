bc7e770276ed90b914bc0d3fc3407596fc863a32
richzago <rich.czago@gmail.com>
2023-01-20 13:58:28 -0300

    feat: updated

--- desafio-frontend-richzago/src/validators/schema.ts
+++ desafio-frontend-richzago/src/validators/schema.ts
@@ -4,14 +4,17 @@ export const calculatorFormSchema = yup.object().shape({
   amount: yup
     .number()
     .required("Campo obrigatório")
+    .typeError("Digite um número")
     .min(1000, "O valor mínimo é de R$ 1.000,00"),
   installments: yup
     .number()
     .required("Campo obrigatório")
+    .typeError("Digite um número")
     .max(12, "O máximo de parcelas é de 12x")
     .min(1, "O mínimo de parcelas é de 1x"),
   mdr: yup
     .number()
     .required("Campo obrigatório")
-    .max(100, "O MDR tem que menor que 100"),
+    .typeError("Digite um número")
+    .max(99, "O MDR tem que menor que 100"),
 });
